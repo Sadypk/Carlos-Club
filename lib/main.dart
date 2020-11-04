c4a65da2ab1c9d361811085ba1d70527a1a4f266
Taz <h.zzzaman@gmail>
2020-11-04 21:04:03 +0600

    folder Structure created

--- lib/main.dart
+++ lib/main.dart
@@ -20,3 +20,42 @@ class MainApp extends StatelessWidget {
   }
 }
 
+
+
+
+
+
+
+//model folder
+class User{
+  String name;
+  User({this.name});
+}
+
+
+//repository
+///backend api calls
+
+
+//view model
+///variables and datas
+///auth
+// 1.login.dart
+// 2.register.dart
+// user
+// 3.userhomescreen.dart
+// 3.1userProfileScreen.dart
+// 3.2userCheckinHistory
+// admin
+// 4.adminHomescreen
+// 4.1adminProfile
+// 4.2membersCheckinHistory
+// 4.3memberAccountdetails
+// 4.5myGroup
+
+
+
+//view
+///screens
+//view.widget
+///shared widgets
